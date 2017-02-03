6b9a3723a58a8ca5ada02028f7ffa12d2f60deba
Vicky Liau <vickyting0910@hotmail.com>
2017-02-02 19:28:31 -0600

    Update spatialindices.py

--- spatialindices.py
+++ spatialindices.py
@@ -6,7 +6,12 @@ from osgeo.gdalconst import *
 from osgeo import ogr
 import pandas as pd
 
-myfile='gridRanQ.gal'
+#Moran's I and Geary's C
+db= pysal.open('gridRan1P.dbf', 'r')
+d = {col: db.by_col(col) for col in db.header}
+table1=pd.DataFrame(d).fillna(0)
+a=num.array(table1['Att'])
+
 myfile='gridRanQ.gal'
 f=open(myfile,'r')
 fline=int(f.readline().replace('\n','').split(' ')[1])
@@ -16,18 +21,6 @@ for line in f:
 
 weights=num.zeros((fline,fline))
 
-for m in range(len(lines)):
-	#lines for the observation IDs and the number of neighbors
-	obs=int(lines[m-1].split(' ')[0]) #get the observation IDs
-	if m%2==1:
-		for n in lines[m].split(' '):
-			weights[obs-1][int(n)-1]=1
-
-#Moran's I and Geary's C
-db= pysal.open('gridRan1.dbf', 'r')
-d = {col: db.by_col(col) for col in db.header}
-table1=pd.DataFrame(d).fillna(0)
-a=num.array(table1['Id'])
 n=len(a)
 sumw=num.sum(weights)
 ymean=num.mean(a)
@@ -49,8 +42,4 @@ print 'PySAL code on MR: '+str(mi.I)
 
 gc = pysal.Geary(y, w)
 print 'PySAL code on GC: '+str(gc.C)
-
-
-#semi-variogram
-
 
